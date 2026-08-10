65fc1b3631c9300f8cbaa36a42be2c35c6552bf9
kelerm <kazakaa@gmail.com>
2026-08-10 19:04:32 +0200

    En fait y'a des trucs a faire

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { GameService } from '../game/services/game.service';
-import { TranslocoModule } from '@jsverse/transloco';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-menu',
@@ -13,7 +13,8 @@ import { TranslocoModule } from '@jsverse/transloco';
 
 export class MenuComponent {
   private router = inject(Router);
-   private gameService = inject(GameService); // <-- Injecte le service ici
+  private gameService = inject(GameService); // <-- Injecte le service ici
+  private transloco = inject(TranslocoService);
 
   public commencerPartie(): void {
      // 1. On déclenche la musique et l'écriture grâce au clic utilisateur (Fin du blocage navigateur !)
@@ -21,5 +22,17 @@ export class MenuComponent {
     
     // 2. On navigue vers l'écran de jeu
     this.router.navigate(['/jeu']);
+  }
+  
+  // Change la langue
+  public setLang(lang: string): void {
+    this.transloco.load(lang).subscribe(() => {
+      this.transloco.setActiveLang(lang);
+    });
+  }
+
+  // Langue actuelle (optionnel, pour le style)
+  public get activeLang(): string {
+    return this.transloco.getActiveLang();
   }
 }
